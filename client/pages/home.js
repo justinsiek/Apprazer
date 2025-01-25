0f6a7516c84126e0efe0b4f14fddfbbb2054ea63
Justin Siek <justinsiek05@gmail.com>
2025-01-24 21:39:17 -0800

    added basic routes

--- client/pages/home.js
+++ client/pages/home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex flex-col justify-center items-center h-screen'>
+        <Link href="/login">Login</Link>
+    </div>
   )
 }
 
--- client/pages/login.js
+++ client/pages/login.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex flex-col justify-center items-center h-screen'>
+        <Link href="/dashboard">Dashboard</Link>
+    </div>
   )
 }
 
